4233400a491eb2327e9181897bd248cd9b47e1cf
flexjnr <felixwanyoike9@gmail.com>
2020-11-05 18:55:31 +0300

    new

---
+++ contactform.php
@@ -0,0 +1,27 @@
+<?php
+    
+
+   $name = $_POST['email'];
+   $visitor_email = $_POST['email'];
+   $message = $_POST['mesage'];
+
+
+
+
+   $email_form = 'felixwanyoike9@gmil.com';
+   $email_subject = 'New form submission'; 
+   $email_body = "User Name: $name.\n".
+                      "User Email: $visitor_email. \n".
+                         "User Message: $message.\n";
+
+    $to = "felixwanyoike9@gmail.com"
+    $headers = "From: &email_form\r\n";
+
+    $hearders  .="Reply-To:visitor_email \r\n ";
+
+    mail ($to,$email_subject,$email_body,$hearders);
+
+    header ("Loction: index.html");                    
+
+
+?>
